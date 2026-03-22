cb599716edf1a5f3c0266b735f31a2d73bbb9404
Aleksander Polanski <s33173@pjwstk.edu.pl>
2026-03-22 21:32:08 +0100

    Added rental class, not finished

---
+++ RentalSystem/Rental.cs
@@ -0,0 +1,23 @@
+﻿namespace RentalSystem;
+
+public class Rental (DateTime rentDate, DateTime dueDate, User user, Device device, double rentalFee)
+{
+    //•	Wypożyczenie - obiekt opisujący moment wypożyczenia, termin zwrotu, faktyczny zwrot oraz wynikające opłaty lub kary.
+    //•	Każde wypożyczenie powinno przechowywać informację: kto, co, kiedy wypożyczył, na jak długo i czy zwrot był terminowy.
+    
+    private static int _nextId = 1;
+    public int Identifier { get; } = _nextId++;
+
+    public User User { get; } = user;
+    public Device Device {get; } = device;
+
+    public DateTime RentDate { get; } = rentDate;
+    public DateTime DueDate { get; } = dueDate;
+    public DateTime? ReturnDate { get; private set; }
+
+    public double RentalFee { get; set; } = rentalFee;
+    public double PriceOfPenalty { get; set; } = 0;
+
+
+
+}
