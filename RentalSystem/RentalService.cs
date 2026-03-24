634409d2d5a8fa74c1ef5fd5a8690938015190b0
Aleksander Polanski <s33173@pjwstk.edu.pl>
2026-03-24 18:51:14 +0100

    Implemented Renting logic

--- RentalSystem/RentalService.cs
+++ RentalSystem/RentalService.cs
@@ -4,12 +4,14 @@ public class RentalService
 {
     private List<User> _users;
     private List<Device> _devices;
+    private List<Rental> _rentals;
 
 
     public RentalService()
     {
         _users = new List<User>();
         _devices = new List<Device>();
+        _rentals = new List<Rental>();
     }
     
     public void AddUser(User user)
@@ -37,8 +39,19 @@ public class RentalService
                 Console.WriteLine(device.Name + ": " + device.Status);
         }
     }
-    
-    
+
+    public void RentDevice(User user, Device device, int days)
+    {
+        if (user.CurrentRentalCount < user.MaxRentalCount && device.Status == DeviceStatus.Available)
+        {
+            _rentals.Add(new Rental(DateTime.Now,  DateTime.Now.AddDays(days), user, device, device.Price));
+            user.CurrentRentalCount++;
+            device.Status = DeviceStatus.Unavailable;
+
+            Console.WriteLine($"User {user.Name} rented {device.Name} and currently has {user.CurrentRentalCount} rentals.");
+        }
+        else Console.WriteLine("Either User exceeded his MaxRentalCount or device is currently unavailable");
+    }
     
     
 }
