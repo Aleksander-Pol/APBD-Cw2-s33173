eae8e514302554838706d43dfacbf2524124d584
Aleksander Polanski <s33173@pjwstk.edu.pl>
2026-03-24 17:45:02 +0100

    Added RentalService class with _devices and _users

---
+++ RentalSystem/RentalService.cs
@@ -0,0 +1,27 @@
+﻿namespace RentalSystem;
+
+public class RentalService
+{
+    private List<User> _users;
+    private List<Device> _devices;
+
+
+    public RentalService()
+    {
+        _users = new List<User>();
+        _devices = new List<Device>();
+    }
+    
+    public void AddUser(User user)
+    {
+        _users.Add(user);
+    }
+
+    public void AddDevice(Device device)
+    {
+        _devices.Add(device);
+    }
+    
+    
+    
+}
